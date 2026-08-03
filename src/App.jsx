3148abf0d389b94c09912eab0b82e0d2739c4f6f
Saminu Awwal <awwalsaminu9@gmail.com>
2026-08-03 15:15:50 +0100

    fix: support trailing-slash and non-trailing-slash variations for all page routes

--- src/App.jsx
+++ src/App.jsx
@@ -195,282 +195,115 @@ function MainContent() {
         } />
         
         {/* About Page */}
-        <Route path="/about" element={
-          <>
-            <PageSeo pageKey="about" />
-            
-            <AboutPage />
-          </>
-        } />
+        <Route path="/about" element={<><PageSeo pageKey="about" /><AboutPage /></>} />
+        <Route path="/about/" element={<><PageSeo pageKey="about" /><AboutPage /></>} />
         
         {/* Certification Pages */}
-        <Route path="/certification/procedure" element={
-          <>
-            <PageSeo pageKey="certificationProcedure" />
-            
-            <CertificationProcedure />
-          </>
-        } />
+        <Route path="/certification/procedure" element={<><PageSeo pageKey="certificationProcedure" /><CertificationProcedure /></>} />
+        <Route path="/certification/procedure/" element={<><PageSeo pageKey="certificationProcedure" /><CertificationProcedure /></>} />
         
-        <Route path="/certification/termination" element={
-          <>
-            <PageSeo pageKey="terminationProcedure" />
-            
-            <TerminationProcedure />
-          </>
-        } />
+        <Route path="/certification/termination" element={<><PageSeo pageKey="terminationProcedure" /><TerminationProcedure /></>} />
+        <Route path="/certification/termination/" element={<><PageSeo pageKey="terminationProcedure" /><TerminationProcedure /></>} />
         
         {/* Policies */}
-        <Route path="/fee-policy" element={
-          <>
-            <PageSeo pageKey="feePolicy" />
-            
-            <FeePolicy />
-          </>
-        } />
+        <Route path="/fee-policy" element={<><PageSeo pageKey="feePolicy" /><FeePolicy /></>} />
+        <Route path="/fee-policy/" element={<><PageSeo pageKey="feePolicy" /><FeePolicy /></>} />
         
-        <Route path="/complaints-committee" element={
-          <>
-            <PageSeo pageKey="complaintsCommittee" />
-            
-            <ComplaintsCommittee />
-          </>
-        } />
+        <Route path="/complaints-committee" element={<><PageSeo pageKey="complaintsCommittee" /><ComplaintsCommittee /></>} />
+        <Route path="/complaints-committee/" element={<><PageSeo pageKey="complaintsCommittee" /><ComplaintsCommittee /></>} />
         
-        <Route path="/provisions-for-the-use-of-hfa-logo" element={
-          <>
-            <PageSeo pageKey="hfaLogoProvisions" />
-            
-            <ProvisionForUseOfHfaLogo />
-          </>
-        } />
+        <Route path="/provisions-for-the-use-of-hfa-logo" element={<><PageSeo pageKey="hfaLogoProvisions" /><ProvisionForUseOfHfaLogo /></>} />
+        <Route path="/provisions-for-the-use-of-hfa-logo/" element={<><PageSeo pageKey="hfaLogoProvisions" /><ProvisionForUseOfHfaLogo /></>} />
         
-        <Route path="/hfa-impartiality-policy" element={
-          <>
-            <PageSeo pageKey="impartialityPolicy" />
-            
-            <HfaImpartialityPolicy />
-          </>
-        } />
+        <Route path="/hfa-impartiality-policy" element={<><PageSeo pageKey="impartialityPolicy" /><HfaImpartialityPolicy /></>} />
+        <Route path="/hfa-impartiality-policy/" element={<><PageSeo pageKey="impartialityPolicy" /><HfaImpartialityPolicy /></>} />
         
-        <Route path="/hfa-quality-policy" element={
-          <>
-            <PageSeo pageKey="qualityPolicy" />
-            
-            <HfaQualityPolicy />
-          </>
-        } />
+        <Route path="/hfa-quality-policy" element={<><PageSeo pageKey="qualityPolicy" /><HfaQualityPolicy /></>} />
+        <Route path="/hfa-quality-policy/" element={<><PageSeo pageKey="qualityPolicy" /><HfaQualityPolicy /></>} />
         
-        <Route path="/hfa-corporate-and-financial-governance" element={
-          <>
-            <PageSeo pageKey="corporateGovernance" />
-            
-            <CoperateAndFinanceGovernment />
-          </>
-        } />
+        <Route path="/hfa-corporate-and-financial-governance" element={<><PageSeo pageKey="corporateGovernance" /><CoperateAndFinanceGovernment /></>} />
+        <Route path="/hfa-corporate-and-financial-governance/" element={<><PageSeo pageKey="corporateGovernance" /><CoperateAndFinanceGovernment /></>} />
         
-        <Route path="/hfa-complaints-appeals-procedure" element={
-          <>
-            <PageSeo pageKey="complaintsAppeals" />
-            
-            <ComplaintsAndAppealsProcedure />
-          </>
-        } />
+        <Route path="/hfa-complaints-appeals-procedure" element={<><PageSeo pageKey="complaintsAppeals" /><ComplaintsAndAppealsProcedure /></>} />
+        <Route path="/hfa-complaints-appeals-procedure/" element={<><PageSeo pageKey="complaintsAppeals" /><ComplaintsAndAppealsProcedure /></>} />
         
-        <Route path="/halal-food-sampling-analysis-policy" element={
-          <>
-            <PageSeo pageKey="foodSampling" />
-            
-            <FoodSamplingAndAnalysisPolicy />
-          </>
-        } />
+        <Route path="/halal-food-sampling-analysis-policy" element={<><PageSeo pageKey="foodSampling" /><FoodSamplingAndAnalysisPolicy /></>} />
+        <Route path="/halal-food-sampling-analysis-policy/" element={<><PageSeo pageKey="foodSampling" /><FoodSamplingAndAnalysisPolicy /></>} />
         
         {/* FAQ */}
-        <Route path="/faqs" element={
-          <>
-            <PageSeo pageKey="faq" />
-            
-            <FAQPage />
-          </>
-        } />
+        <Route path="/faqs" element={<><PageSeo pageKey="faq" /><FAQPage /></>} />
+        <Route path="/faq" element={<><PageSeo pageKey="faq" /><FAQPage /></>} />
         
         {/* Contact */}
-        <Route path="/contact" element={
-          <>
-            <PageSeo pageKey="contact" />
-            
-            <Contact />
-          </>
-        } />
+        <Route path="/contact" element={<><PageSeo pageKey="contact" /><Contact /></>} />
+        <Route path="/contact/" element={<><PageSeo pageKey="contact" /><Contact /></>} />
         
         {/* Events */}
-        <Route path="/events/hfa-planning-2025" element={
-          <>
-            <PageSeo pageKey="eventPlanning2025" />
-            
-            <HFAEventPlanning2025 />
-          </>
-        } />
-        
-        <Route path="/events/hfa-webinar-2020" element={
-          <>
-            <PageSeo pageKey="webinar2020" />
-            
-            <HFAWebinar2020 />
-          </>
-        } />
-        
-        <Route path="/events/ulemah-meeting-jan-2019/" element={
-          <>
-            <PageSeo pageKey="ulemahMeeting2019" />
-            
-            <HFAUleemahMeeting2019 />
-          </>
-        } />
+        <Route path="/events/hfa-planning-2025" element={<><PageSeo pageKey="eventPlanning2025" /><HFAEventPlanning2025 /></>} />
+        <Route path="/events/hfa-webinar-2020" element={<><PageSeo pageKey="webinar2020" /><HFAWebinar2020 /></>} />
+        <Route path="/events/ulemah-meeting-jan-2019" element={<><PageSeo pageKey="ulemahMeeting2019" /><HFAUleemahMeeting2019 /></>} />
+        <Route path="/events/ulemah-meeting-jan-2019/" element={<><PageSeo pageKey="ulemahMeeting2019" /><HFAUleemahMeeting2019 /></>} />
         
         {/* HHIC Events */}
-        <Route path="/events/hhic2019/" element={
-          <>
-            <PageSeo pageKey="hhic2019" />
-            
-            <HHIC2019 />
-          </>
-        } />
+        <Route path="/events/hhic2019" element={<><PageSeo pageKey="hhic2019" /><HHIC2019 /></>} />
+        <Route path="/events/hhic2019/" element={<><PageSeo pageKey="hhic2019" /><HHIC2019 /></>} />
         
-        <Route path="/events/hhic2018/" element={
-          <>
-            <PageSeo pageKey="hhic2018" />
-            
-            <HHIC2018 />
-          </>
-        } />
+        <Route path="/events/hhic2018" element={<><PageSeo pageKey="hhic2018" /><HHIC2018 /></>} />
+        <Route path="/events/hhic2018/" element={<><PageSeo pageKey="hhic2018" /><HHIC2018 /></>} />
         
-        <Route path="/events/hhic2017/" element={
-          <>
-            <PageSeo pageKey="hhic2017" />
-            
-            <HHIC2017 />
-          </>
-        } />
+        <Route path="/events/hhic2017" element={<><PageSeo pageKey="hhic2017" /><HHIC2017 /></>} />
+        <Route path="/events/hhic2017/" element={<><PageSeo pageKey="hhic2017" /><HHIC2017 /></>} />
         
-        <Route path="/events/hhic2016/" element={
-          <>
-            <PageSeo pageKey="hhic2016" />
-            
-            <HHIC2016 />
-          </>
-        } />
+        <Route path="/events/hhic2016" element={<><PageSeo pageKey="hhic2016" /><HHIC2016 /></>} />
+        <Route path="/events/hhic2016/" element={<><PageSeo pageKey="hhic2016" /><HHIC2016 /></>} />
         
-        <Route path="/events/hhic2015/" element={
-          <>
-            <PageSeo pageKey="hhic2015" />
-            
-            <HHIC2015 />
-          </>
-        } />
+        <Route path="/events/hhic2015" element={<><PageSeo pageKey="hhic2015" /><HHIC2015 /></>} />
+        <Route path="/events/hhic2015/" element={<><PageSeo pageKey="hhic2015" /><HHIC2015 /></>} />
         
         {/* Muslim Expo */}
-        <Route path="/events/muslimlife2016/" element={
-          <>
-            <PageSeo pageKey="muslimExpo" />
-            
-            <MuslimExpo />
-          </>
-        } />
+        <Route path="/events/muslimlife2016" element={<><PageSeo pageKey="muslimExpo" /><MuslimExpo /></>} />
+        <Route path="/events/muslimlife2016/" element={<><PageSeo pageKey="muslimExpo" /><MuslimExpo /></>} />
         
         {/* Find More */}
-        <Route path="/find-more-halal-food-authourity/" element={
-          <>
-            <PageSeo pageKey="findMore" />
-            
-            <FindMoreHalalFoodAuthority />
-          </>
-        } />
+        <Route path="/find-more-halal-food-authourity" element={<><PageSeo pageKey="findMore" /><FindMoreHalalFoodAuthority /></>} />
+        <Route path="/find-more-halal-food-authourity/" element={<><PageSeo pageKey="findMore" /><FindMoreHalalFoodAuthority /></>} />
         
         {/* Products Categories */}
-        <Route path="/food-and-beverages/" element={
-          <>
-            <PageSeo pageKey="foodBeverages" />
-            
-            <FoodAndBeverages />
-          </>
-        } />
+        <Route path="/food-and-beverages" element={<><PageSeo pageKey="foodBeverages" /><FoodAndBeverages /></>} />
+        <Route path="/food-and-beverages/" element={<><PageSeo pageKey="foodBeverages" /><FoodAndBeverages /></>} />
         
-        <Route path="/cosmetics/" element={
-          <>
-            <PageSeo pageKey="cosmetics" />
-            
-            <Cosmetics />
-          </>
-        } />
+        <Route path="/cosmetics" element={<><PageSeo pageKey="cosmetics" /><Cosmetics /></>} />
+        <Route path="/cosmetics/" element={<><PageSeo pageKey="cosmetics" /><Cosmetics /></>} />
         
-        <Route path="/pharmaceuticals/" element={
-          <>
-            <PageSeo pageKey="pharmaceuticals" />
-            
-            <Pharmaceuticals />
-          </>
-        } />
+        <Route path="/pharmaceuticals" element={<><PageSeo pageKey="pharmaceuticals" /><Pharmaceuticals /></>} />
+        <Route path="/pharmaceuticals/" element={<><PageSeo pageKey="pharmaceuticals" /><Pharmaceuticals /></>} />
         
-        <Route path="/ingredients/" element={
-          <>
-            <PageSeo pageKey="ingredients" />
-            
-            <Ingredients />
-          </>
-        } />
+        <Route path="/ingredients" element={<><PageSeo pageKey="ingredients" /><Ingredients /></>} />
+        <Route path="/ingredients/" element={<><PageSeo pageKey="ingredients" /><Ingredients /></>} />
         
-        <Route path="/additives-aromas/" element={
-          <>
-            <PageSeo pageKey="additivesAromas" />
-            
-            <AdditivesAndAromas />
-          </>
-        } />
+        <Route path="/additives-aromas" element={<><PageSeo pageKey="additivesAromas" /><AdditivesAndAromas /></>} />
+        <Route path="/additives-aromas/" element={<><PageSeo pageKey="additivesAromas" /><AdditivesAndAromas /></>} />
         
-        <Route path="/packaging-materials/" element={
-          <>
-            <PageSeo pageKey="packaging" />
-            
-            <PackagingMaterials />
-          </>
-        } />
+        <Route path="/packaging-materials" element={<><PageSeo pageKey="packaging" /><PackagingMaterials /></>} />
+        <Route path="/packaging-materials/" element={<><PageSeo pageKey="packaging" /><PackagingMaterials /></>} />
         
         {/* UK Parliament */}
-        <Route path="/uk-parliament-debate-on-banning-non-stun-slaughter-on-9th-may-hfa-perspective/" element={
-          <>
-            <PageSeo pageKey="ukParliamentDebate" />
-            
-            <UKParliamentDebateOnBanning />
-          </>
-        } />
+        <Route path="/uk-parliament-debate-on-banning-non-stun-slaughter-on-9th-may-hfa-perspective" element={<><PageSeo pageKey="ukParliamentDebate" /><UKParliamentDebateOnBanning /></>} />
+        <Route path="/uk-parliament-debate-on-banning-non-stun-slaughter-on-9th-may-hfa-perspective/" element={<><PageSeo pageKey="ukParliamentDebate" /><UKParliamentDebateOnBanning /></>} />
         
         {/* Our Clients */}
-        <Route path="/our-clients/" element={
-          <>
-            <PageSeo pageKey="ourClients" />
-            
-            <OurClients />
-          </>
-        } />
+        <Route path="/our-clients" element={<><PageSeo pageKey="ourClients" /><OurClients /></>} />
+        <Route path="/our-clients/" element={<><PageSeo pageKey="ourClients" /><OurClients /></>} />
         
         {/* Exporting */}
-        <Route path="/exporting-your-products-to-indonesia/" element={
-          <>
-            <PageSeo pageKey="exportIndonesia" />
-            
-            <ExportingYourProductsToIndonesia />
-          </>
-        } />
+        <Route path="/exporting-your-products-to-indonesia" element={<><PageSeo pageKey="exportIndonesia" /><ExportingYourProductsToIndonesia /></>} />
+        <Route path="/exporting-your-products-to-indonesia/" element={<><PageSeo pageKey="exportIndonesia" /><ExportingYourProductsToIndonesia /></>} />
         
         {/* Events List */}
-        <Route path="/our-events/" element={
-          <>
-            <PageSeo pageKey="eventsList" />
-            
-            <Events />
-          </>
-        } />
+        <Route path="/our-events" element={<><PageSeo pageKey="eventsList" /><Events /></>} />
+        <Route path="/our-events/" element={<><PageSeo pageKey="eventsList" /><Events /></>} />
+        <Route path="/events" element={<><PageSeo pageKey="eventsList" /><Events /></>} />
+        <Route path="/events/" element={<><PageSeo pageKey="eventsList" /><Events /></>} />
 
         {/* NotFound page */}
 
